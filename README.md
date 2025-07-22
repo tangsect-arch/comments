c8bcf35c0a0e2ce54519ca89a37da2064c7ab333
Sabari <sabari288@gmail.com>
2025-07-22 09:51:34 +0530

    updated sorting logic and readme

--- README.md
+++ README.md
@@ -5,3 +5,32 @@ sort comments
 # swagger file
 
 http://localhost:5000/api-docs/#/
+
+# features
+
+register
+signin
+
+create video
+list all video
+list video by id
+
+create comments against a video
+sort comments
+list comment by id
+update comment
+delete comment
+like dislike comment
+remove like
+
+create reply
+update reply
+delete reply
+like dislike reply
+remove like/dislike
+
+comments can be sorted based on
+created at
+rating
+
+rating depends on no of likes dislikes and replies
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -36,14 +36,14 @@ export const hashPassword = async (password: string): Promise<string> => {
 
 export const isPasswordValid = async (
   plainText: string,
-  hashed: string,
+  hashed: string
 ): Promise<boolean> => {
   return await bcrypt.compare(plainText, hashed);
 };
 
 export const isPasswordMatch = async (
   password: string,
-  userPassword: string,
+  userPassword: string
 ): Promise<boolean> => {
   const isMatch = await bcrypt.compare(password, userPassword);
   return isMatch;
@@ -57,20 +57,12 @@ export const getRating = (
   created_at: Date,
   likes: number,
   dislikes: number,
-  replies: number,
+  replies: number
 ): number => {
-  const now = new Date();
-  created_at = new Date(created_at);
-  const ageInHours = (now.getTime() - created_at.getTime()) / (1000 * 60 * 60);
-
   const likeScore = likes * 1.5;
   const dislikePenalty = dislikes * 1.0;
   const replyBonus = replies * 1.2;
-
-  const timeDecay = 1 / Math.log2(ageInHours + 2);
-
-  const rawScore = (likeScore + replyBonus - dislikePenalty) * timeDecay;
-
+  const rawScore = likeScore + replyBonus - dislikePenalty;
   const clampedScore = Math.max(1.0, Math.min(rawScore, 100.0));
 
   return parseFloat(clampedScore.toFixed(2));
